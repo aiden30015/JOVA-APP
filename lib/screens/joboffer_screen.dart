bc6e0dd667922b4baff0520cec4e8707f0ae0f29
LeeJueon <loolloo080313@gmail.com>
2024-10-14 17:14:53 +0900

    구인 스크린 코드입니다(구현중)

---
+++ lib/screens/joboffer_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class JobofferScreen extends StatefulWidget {
+  const JobofferScreen({super.key});
+
+  @override
+  State<JobofferScreen> createState() => _JobofferScreenState();
+}
+
+class _JobofferScreenState extends State<JobofferScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
