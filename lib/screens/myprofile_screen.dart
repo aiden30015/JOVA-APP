fd0f13ece57467ed856c818c9a8eb5c426de62d9
LeeJueon <loolloo080313@gmail.com>
2024-10-14 17:15:26 +0900

    내 정보 스크린 코드입니다(구현중)

---
+++ lib/screens/myprofile_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class MyprofileScreen extends StatefulWidget {
+  const MyprofileScreen({super.key});
+
+  @override
+  State<MyprofileScreen> createState() => _MyprofileScreenState();
+}
+
+class _MyprofileScreenState extends State<MyprofileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
