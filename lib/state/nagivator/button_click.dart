243948a4f557f0c747a8ffdfe5d958849d4a6453
LeeJueon <loolloo080313@gmail.com>
2024-10-15 19:58:01 +0900

    메인 되돌아가기 버튼 기능 구현했습니다

--- lib/state/nagivator/button_click.dart
+++ lib/state/nagivator/button_click.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:jovajovajova/screens/main_screen.dart';
 import '../../screens/joboffer_screen.dart';
 import '../../screens/jobsearch_screen.dart';
 import '../../screens/myprofile_screen.dart';
+class ButtonAnimation{
+  static AnimationController createController(TickerProvider vsync, {Duration duration = const Duration(seconds: 1)}) {
+    return AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: vsync,
+    );
+  }
 
+  static Animation<double> createScaleAnimation(AnimationController controller, {double begin = 1.0, double end = 1.5}) {
+    return Tween<double>(begin: begin, end: end).animate(controller);
+  }
+
+  static void startAnimation(AnimationController controller) {
+    controller.forward();
+  }
+
+  static void reverseAnimation(AnimationController controller) {
+    controller.reverse();
+  }
+}
 class ButtonActions {
   static void jobSerachButton(BuildContext context) {
     Navigator.push(
@@ -22,6 +42,13 @@ class ButtonActions {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => const MyprofileScreen()),
+    );
+  }
+
+  static void backmainbutton(BuildContext context){
+    Navigator.push(
+        context,
+      MaterialPageRoute(builder: (context) => const MainScreen()),
     );
   }
 }
