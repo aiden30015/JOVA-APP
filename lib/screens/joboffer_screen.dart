dfe7ecfcd2c662a4825c41358bec94de61ed2480
LeeJueon <loolloo080313@gmail.com>
2024-10-15 20:00:18 +0900

    검색바 추가 및 위젯 배치 완료했습니다

--- lib/screens/joboffer_screen.dart
+++ lib/screens/joboffer_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:jovajovajova/widget/top_container.dart';
+import 'package:jovajovajova/widget/main_backbutton.dart';
+import 'package:jovajovajova/widget/job_serach.dart';
 
 class JobofferScreen extends StatefulWidget {
   const JobofferScreen({super.key});
@@ -8,8 +12,36 @@ class JobofferScreen extends StatefulWidget {
 }
 
 class _JobofferScreenState extends State<JobofferScreen> {
+
+  bool jobofferclick = true;
+  bool jobsearchclick = false;
+
+  void ButtonClickState(bool onclick){
+    setState(() {
+      onclick = !onclick;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            TopContainer(),
+            Positioned(
+              top: 30,
+              left: 0,
+              child: MainBackbutton(),
+            ),
+            Positioned(
+                top: 250,
+                left: 0,
+                child: JobSerach(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
